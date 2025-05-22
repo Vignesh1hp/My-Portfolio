5a500765e62259cda55a6c7515672c6edeefacda
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2025-05-22 20:54:13 +0530

    created contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Contact = () => {
+  return (
+    <section className="flex flex-col bg-primary px-5 py-32 text-white">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl border-b-4 border-b-[#2d2b77] mb-5 w-[135px] font-bold">Contact</h1>
+        <p className="pb-5">If you want to discuss more in detail,please contact me</p>
+        <p className="py-2"><span className="font-bold">Email : </span>vigneshkumarb.sc2004@gmail.com</p>
+        <p className="py-2"><span className="font-bold">Phone : </span>7010722215</p>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
